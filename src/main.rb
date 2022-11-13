7e09c537dae33b83598d814eba1f53068d1405b3
Gustavo <gustavopchaves@hotmail.com>
2022-11-12 21:42:15 -0300

    🎨 Removido código não funcional e refatoração para casos de erro

--- src/main.rb
+++ src/main.rb
@@ -26,7 +26,7 @@ def select_list(text,options)
     selected_index = gets.chomp.to_i - 1
     selected_option = options[selected_index]
 
-    return Screen::MAIN if selected_option == nil
+    return nil if selected_option.nil?
 
     next_page = selected_option[:value]
 end
@@ -40,11 +40,13 @@ def products_page
     text = "Selecione a opção desejada: "
     product_value = select_list(text, Screen::PRODUCTS_PAGE_OPTIONS)
 
+    return 0 if product_value.nil?
+
     print "Digite a quantidade desejada: "
     quantity = gets.chomp.to_i
     price = product_value * quantity
 
-    return Screen::SUBTOTAL, price
+    return price
 end
 
 def subtotal_page subtotal
@@ -70,13 +72,16 @@ def main
         when Screen::MAIN
             current_screen = main_page
         when Screen::PRODUCTS
-            current_screen, price = products_page
+            price = products_page
             subtotal += price
+            current_screen = Screen::SUBTOTAL
         when Screen::SUBTOTAL
             current_screen = subtotal_page subtotal
         when Screen::EXIT
             exit_page
-            quit = true  
+            quit = true
+        else
+            break
         end
     end
 end
